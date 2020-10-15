fa953b4bc28af6f42bce0c7931eafc123abc919a
taylor-curran <taylorcurranagtc@gmail.com>
2020-10-15 09:16:51 -0400

    Adding initial predictive model

--- twit_compare/models.py
+++ twit_compare/models.py
@@ -4,22 +4,23 @@
 
 from flask_sqlalchemy import SQLAlchemy
 
+# Create an Instance of a DB that has Classes for Tables
+# and Objects for rows
 DB = SQLAlchemy()
 
 # To most of development world, model refers to like a 
 # data model or data structure.
-
-
 
 class User(DB.Model):
     """Twitter users corresponding to Tweets."""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
     # Tweet IDs are ordinal ints, so can be used to fetch only more recent
+    # User's most recent tweet
     newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
-        return '-User {}-'.format(self.name)
+        return '--User {}--'.format(self.name)
 
 
 class Tweet(DB.Model):
@@ -28,10 +29,14 @@ class Tweet(DB.Model):
     text = DB.Column(DB.Unicode(300)) # Allows for Text + Links
     # We need the DB to somehow store the embedding.
     # embedding = DB.Column(DB.PickleType, nullable=False)
+
+    # Holds word embeddings
+    vect = DB.Column(DB.PickleType, nullable=False)
+
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
-        return '-Tweet {}-'.format(self.text)
+        return '--Tweet {}--'.format(self.text)
 
 
